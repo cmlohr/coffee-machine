317ad0658da55a8691be2e58a648e1cefaffb7f5
Colleen <cmlohr@outlook.com>
2020-11-16 22:25:56 -0600

    final changes made for v1.0 coffee machine

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ MENU = {
 }
 
 resources = {
-    "water": 0,
+    "water": 300,
     "milk": 200,
     "coffee": 100,
 }
@@ -38,34 +38,29 @@ def menu():
     print("Espresso\nLatte\nCappuccino")
     print("---------------------------")
     print("   type your selection")
+
+
 def kiosk_help():
-    print("commands:")
+    print("Instructions")
+    print("Type your order, eg: espresso")
+    print("Other kiosk commands:")
     print("---------------------------")
-    print("'report' displays the current supply level")
+    print("'report' displays a report of\nthe stock level and bank")
     print("'off' turns off machine")
     print("---------------------------")
 
-    # print("Here is your {drink} ☕ enjoy!")
-    # print("Sorry that's not enough money.  Money Refunded.")
-# TODO 2 Check resources sufficient to make drink order.
-
-
-# needs to print the remaining resources
-# provides change
-# calculates change
-# refunds money if not enough
-# Thank you for shopping at this kiosk
-# deduct resources add money into bank
 
 def kiosk():
-    menu()
-
+    print("---------------------------")
     nickle = 0.05
-    dime = 0.1
+    dime = 0.10
     penny = 0.01
-    quarter = .25
-    wallet = 0
-    bank = 0
+    quarter = 0.25
+    wallet = 0.00
+    bank = 0.00
+    water_supply = resources.get('water')
+    milk_supply = resources.get('milk')
+    coffee_supply = resources.get('coffee')
     cont = True
     while cont:
         # Separating out the values
@@ -80,54 +75,57 @@ def kiosk():
         espr_rec = espr_drink.get('ingredients')
         latte_rec = latte_drink.get('ingredients')
         capp_rec = capp_drink.get('ingredients')
-        # Resources
-        water_supply = resources.get('water')
-        milk_supply = resources.get('milk')
-        coffee_supply = resources.get('coffee')
-        # input output conditions
+        menu()
+
+
         order = input("What would you like to order?:\n> ").lower()
         if order == "help":
             kiosk_help()
         elif order == "report":
-            print("_____Supply_____")
+            print("----------report-----------")
             print(f"Water Level: {water_supply}ml")
             print(f"Milk Level: {milk_supply}ml")
             print(f"Coffee Level: {coffee_supply}g")
-            print(f"Bank: ${bank}")
-            print("________________")
+            form_bank = "{:.2f}".format(bank)
+            print(f"Bank: ${form_bank}")
+            print("---------------------------")
 
         elif order == "espresso":
             if espr_rec.get('water') > water_supply:
                 print("Sorry there isn't enough water.\nPlease contact management.")
+                print("---------------------------")
                 kiosk()
             if espr_rec.get('coffee') > coffee_supply:
                 print("Sorry there isn't enough coffee grounds.\nPlease contact management.")
+                print("---------------------------")
                 kiosk()
-            if espr_rec.get('milk') > milk_supply:
-                print("Sorry there isn't enough milk.\nPlease contact management.")
-                kiosk()
-            total = 0
+            total = 0.00
             total += price_espr
             water_supply -= espr_rec.get('water')
             coffee_supply -= espr_rec.get('coffee')
             form_total = "{:.2f}".format(total)
             print(f"your total is: {form_total}")
             print("Please insert coins:")
-            q_coin = float(input("How many quarters?\n> "))
+            q_coin = float(input("How many Quarters?\n> "))
             wallet += q_coin * quarter
-            print(wallet)
-            d_coin = float(input("How many dimes?\n> "))
+            d_coin = float(input("How many Dimes?\n> "))
             wallet += d_coin * dime
-            n_coin = float(input("How many nickles?\n> "))
+            n_coin = float(input("How many Nickles?\n> "))
             wallet += n_coin * nickle
-            p_coin = float(input("How many penny's?\n> "))
+            p_coin = float(input("How many Pennies?\n> "))
             wallet += p_coin * penny
+            if wallet < total:
+                print("---------------------------")
+                print("Sorry, that is not enough.\nYour transaction has been\ncanceled and money refunded.")
+                print("---------------------------")
+                kiosk()
             change = wallet - total
             form_change = "{:.2f}".format(change)
             bank += total
-            print(f"bank test: {bank}") #
-            print(f"change test: Here is {form_change} in change.")
-            drink = "Espresso"
+            print(f"Your change is ${form_change}")
+            print(f"Here is your Espresso.️Enjoy!")
+            print("Thank you for shopping with us!")
+            print("---------------------------")
 
         elif order == "latte":
             if latte_rec.get('water') > water_supply:
@@ -139,29 +137,32 @@ def kiosk():
             if latte_rec.get('milk') > milk_supply:
                 print("Sorry there isn't enough milk.\nPlease contact management.")
                 kiosk()
-            total = 0
+            total = 0.00
             total += price_latte
-            water_supply -= latte_rec.get('water')
-            coffee_supply -= latte_rec.get('coffee')
-            milk_supply -= latte_rec.get('milk')
+            water_supply = water_supply - latte_rec.get('water')
+            coffee_supply = coffee_supply - latte_rec.get('coffee')
+            milk_supply = milk_supply - latte_rec.get('milk')
             form_total = "{:.2f}".format(total)
             print(f"your total is: {form_total}")
             print("Please insert coins:")
-            q_coin = float(input("How many quarters?\n> "))
+            q_coin = float(input("How many Quarters?\n> "))
             wallet += q_coin * quarter
-            print(wallet)
-            d_coin = float(input("How many dimes?\n> "))
+            d_coin = float(input("How many Dimes?\n> "))
             wallet += d_coin * dime
-            n_coin = float(input("How many nickles?\n> "))
+            n_coin = float(input("How many Nickles?\n> "))
             wallet += n_coin * nickle
-            p_coin = float(input("How many penny's?\n> "))
+            p_coin = float(input("How many Pennies?\n> "))
             wallet += p_coin * penny
+            if wallet < total:
+                print("---------------------------")
+                print("Sorry, that is not enough.\nYour transaction has been\ncanceled and money refunded.")
+                kiosk()
             change = wallet - total
             form_change = "{:.2f}".format(change)
             bank += total
-            print(f"bank test: {bank}")  #
-            print(f"change test: Here is ${form_change} in change.")
-            drink = "Latte"
+            print(f"Your change is ${form_change}")
+            print(f"Here is your Latte.️Enjoy!")
+            print("Thank you for shopping with us!")
 
         elif order == "cappuccino":
             if capp_rec.get('water') > water_supply:
@@ -179,29 +180,32 @@ def kiosk():
             coffee_supply -= capp_rec.get('coffee')
             milk_supply -= capp_rec.get('milk')
             form_total = "{:.2f}".format(total)
-            print(f"your total is: {form_total}")
+            print(f"your total is: ${form_total}")
             print("Please insert coins:")
-            q_coin = float(input("How many quarters?\n> "))
+            q_coin = float(input("How many Quarters?\n> "))
             wallet += q_coin * quarter
-            print(wallet)
-            d_coin = float(input("How many dimes?\n> "))
+            d_coin = float(input("How many Dimes?\n> "))
             wallet += d_coin * dime
-            n_coin = float(input("How many nickles?\n> "))
+            n_coin = float(input("How many Nickles?\n> "))
             wallet += n_coin * nickle
-            p_coin = float(input("How many penny's?\n> "))
+            p_coin = float(input("How many Pennies?\n> "))
             wallet += p_coin * penny
+            if wallet < total:
+                print("---------------------------")
+                print("Sorry, that is not enough.\nYour transaction has been\ncanceled and money refunded.")
+                kiosk()
             change = wallet - total
             form_change = "{:.2f}".format(change)
             bank += total
-            print(bank)
-            print(f"Here is {form_change} in change.")
-            drink = "Cappuccino"
+            print(f"Your change is ${form_change}")
+            print(f"Here is your Cappuccino. Enjoy!")
+            print("Thank you for shopping with us!")
 
         elif order == "off":
             cont = False
-
         else:
             print("Invalid Input")
             kiosk()
+
+
 kiosk()
- # print(f"Supply Testing\nWater: {water_supply} | Coffee: {coffee_supply} | Milk: {milk_supply}")
